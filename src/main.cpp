bcde56745508a2cb76683f0f0c46e21623f90aa4
ungoodman <suchanej.19@gmail.com>
2024-05-20 17:14:14 +0700

    example keypad lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,39 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <I2CKeyPad.h> 
+#include <I2CKeyPad.h>
 #include <Keypad.h>
 
-I2CKeyPad keypad(0x20);                  //  เป็นคำสั่งเก็บค่า address ของ keypad address = 0x20
-LiquidCrystal_I2C lcd(0x27, 16, 2);      //  เป็นการตั้งค่า ของจอ Lcd (0*27 คือขนาดของจอ,16 ตัวอักษร ,2 บรรทัด)
+I2CKeyPad keypad(0x20);             //  เป็นคำสั่งเก็บค่า address ของ keypad address = 0x20
+LiquidCrystal_I2C lcd(0x27, 16, 2); //  เป็นการตั้งค่า ของจอ Lcd (0*27 คือขนาดของจอ,16 ตัวอักษร ,2 บรรทัด)
 
-char keymap[19] = "123A456B789C*0#DNF";  //  เป็นคำสั่งใช้ตัวแปร char โดยชื่อ keymap เป็นตัวเก็บจำนวนไว้ที่ตัวแปร ของ array
+char keymap[19] = "123A456B789C*0#DNF"; //  เป็นคำสั่งใช้ตัวแปร char โดยชื่อ keymap เป็นตัวเก็บจำนวนไว้ที่ตัวแปร ของ array
 
-                                         // สร้างออบเจ็ค Keypad_I2C
- 
+// สร้างออบเจ็ค Keypad_I2C
+
 String inputTime = "";
 
-void setup() {                           // เริ่มต้นการทำงานของ I2C
- 
-  Wire.begin();
-  Wire.setClock(400000);                 //  เป็นคำสั่งตั้งค่าความเร็วในการสื่อสาร (400000 fast mode )                                    // เริ่มต้นการทำงานของ LCD
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.setCursor(0, 1);
-  Serial.begin(115200);
-   if (keypad.begin() == false)          //  ถ้า (keypad.begin เป็นการตรวจสอบว่าสื่อสารกันได้) keypad เป็น เท็จ
+void setup()
+{ // เริ่มต้นการทำงานของ I2C
+
+    Wire.begin();
+    Wire.setClock(400000); //  เป็นคำสั่งตั้งค่าความเร็วในการสื่อสาร (400000 fast mode )
+
+    lcd.init(); // เริ่มต้นการทำงานของ LCD
+    lcd.backlight();
+
+    //   lcd.setCursor(0, 0);
+    //   lcd.setCursor(0, 1);
+
+    Serial.begin(115200);
+
+    if (keypad.begin() == false) //  ถ้า (keypad.begin เป็นการตรวจสอบว่าสื่อสารกันได้) keypad เป็น เท็จ
     {
         lcd.println("\nkeypadError");
         while (1)
             ; //  เป็นคำสั่งทำซํ้าตลอดไปไม่หยุด
     }
-      
-     keypad.loadKeyMap(keymap);          //  เป็นการตั้งค่า layout ของ keypad เป็นการดึงค่าจาก keymap มา
+
+    keypad.loadKeyMap(keymap); //  เป็นการตั้งค่า layout ของ keypad เป็นการดึงค่าจาก keymap มา
 }
 
 char getkeypadPressed()
@@ -43,28 +48,72 @@ char getkeypadPressed()
         return ' ';
     }
 }
-void loop() {
-  char re = getkeypadPressed();
-  
-  if (re) {
-    if (re == '#') {
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Time:");
-      lcd.setCursor(0, 1);
-      lcd.print(inputTime);
-      inputTime = "";
-    } else if (re == '*') {
-      inputTime = "";
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Enter Time:");
-      lcd.setCursor(0, 1);
-    } else {
-      inputTime += re;
-      lcd.setCursor(0, 1);
-      lcd.print(inputTime);
-    }
-  }
-}
 
+String lastestValue;
+
+void loop()
+{
+    if (keypad.isPressed())
+    {
+        char keypadValue = keypad.getChar();
+        Serial.println("Input from Keypad: " + keypadValue);
+
+        if (keypadValue == '#')
+        {
+            /* code */
+        }
+        else if (keypadValue == '*')
+        {
+            /* code */
+        }
+        else if (keypadValue >= '0' && keypadValue <= '9')
+        {
+            lastestValue = inputTime;
+            inputTime += keypadValue;
+        }
+        else
+        {
+            Serial.println("Menu: " + keypadValue);
+        }
+    }
+
+    if (millis() % 100 == 0 && inputTime != lastestValue)
+    {
+        lcd.clear();
+
+        lcd.setCursor(0, 0);
+        lcd.print("   Set  Time   ");
+
+        lcd.setCursor(0, 1);
+        lcd.print(inputTime);
+    }
+
+    // char re = getkeypadPressed();
+
+    // if (re)
+    // {
+    //     if (re == '#')
+    //     {
+    //         lcd.clear();
+    //         lcd.setCursor(0, 0);
+    //         lcd.print("Time:");
+    //         lcd.setCursor(0, 1);
+    //         lcd.print(inputTime);
+    //         inputTime = "";
+    //     }
+    //     else if (re == '*')
+    //     {
+    //         inputTime = "";
+    //         lcd.clear();
+    //         lcd.setCursor(0, 0);
+    //         lcd.print("Enter Time:");
+    //         lcd.setCursor(0, 1);
+    //     }
+    //     else
+    //     {
+    //         inputTime += re;
+    //         lcd.setCursor(0, 1);
+    //         lcd.print(inputTime);
+    //     }
+    // }
+}
