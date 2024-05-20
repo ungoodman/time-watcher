7a01da2869bc9137ae6529a979f63c489c1a5f13
thanakornkmu <thanakorngc@gmail.com>
2024-05-20 15:59:22 +0700

    ทดลอง02 keypad_lcd

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,33 @@
 #include <I2CKeyPad.h> 
 #include <Keypad.h>
 
-I2CKeyPad keypad(0x20);             //  เป็นคำสั่งเก็บค่า address ของ keypad address = 0x20
-LiquidCrystal_I2C lcd(0x27, 16, 2); //  เป็นการตั้งค่า ของจอ Lcd (0*27 คือขนาดของจอ,16 ตัวอักษร ,2 บรรทัด)
+I2CKeyPad keypad(0x20);                  //  เป็นคำสั่งเก็บค่า address ของ keypad address = 0x20
+LiquidCrystal_I2C lcd(0x27, 16, 2);      //  เป็นการตั้งค่า ของจอ Lcd (0*27 คือขนาดของจอ,16 ตัวอักษร ,2 บรรทัด)
 
-char keymap[19] = "123A456B789C*0#DNF"; //  เป็นคำสั่งใช้ตัวแปร char โดยชื่อ keymap เป็นตัวเก็บจำนวนไว้ที่ตัวแปร ของ array
+char keymap[19] = "123A456B789C*0#DNF";  //  เป็นคำสั่งใช้ตัวแปร char โดยชื่อ keymap เป็นตัวเก็บจำนวนไว้ที่ตัวแปร ของ array
 
-                                       // สร้างออบเจ็ค Keypad_I2C
-
+                                         // สร้างออบเจ็ค Keypad_I2C
+ 
 String inputTime = "";
 
-void setup() {                        // เริ่มต้นการทำงานของ I2C
+void setup() {                           // เริ่มต้นการทำงานของ I2C
  
   Wire.begin();
-                                      // เริ่มต้นการทำงานของ LCD
-  lcd.begin();
+  Wire.setClock(400000);                 //  เป็นคำสั่งตั้งค่าความเร็วในการสื่อสาร (400000 fast mode )                                    // เริ่มต้นการทำงานของ LCD
+  lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Enter Time:");
   lcd.setCursor(0, 1);
+  Serial.begin(115200);
+   if (keypad.begin() == false)          //  ถ้า (keypad.begin เป็นการตรวจสอบว่าสื่อสารกันได้) keypad เป็น เท็จ
+    {
+        lcd.println("\nkeypadError");
+        while (1)
+            ; //  เป็นคำสั่งทำซํ้าตลอดไปไม่หยุด
+    }
       
-  keypad.begin();                     // เริ่มต้นการทำงานของ Keypad
+  keypad.begin();                         // เริ่มต้นการทำงานของ Keypad
+  keypad.loadKeyMap(keymap);              //  เป็นการตั้งค่า layout ของ keypad เป็นการดึงค่าจาก keymap มา
 }
 
 char getkeypadPressed()
@@ -49,7 +56,7 @@ void loop() {
       lcd.print(inputTime);
       inputTime = "";
     } else if (re == '*') {
-      inputTime = "";String inputTime = "";
+      inputTime = "";
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Enter Time:");
