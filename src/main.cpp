450b4cc799c9fbbb8b2fc558ce920c76aea18c73
ungoodman <suchanej.19@gmail.com>
2024-06-08 01:54:30 +0700

    Complete Test

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@
 #include <RF24.h>
 
 RF24 radio(8, 7);
-I2CKeyPad keypad(0x20);                 //  เป็นคำสั่งเก็บค่า address ของ keypad address = 0x20
-LiquidCrystal_I2C lcd(0x27, 16, 2);     //  เป็นการตั้งค่า ของจอ Lcd (0*27 คือขนาดของจอ,16 ตัวอักษร ,2 บรรทัด)
+I2CKeyPad keypad(0x20);             //  เป็นคำสั่งเก็บค่า address ของ keypad address = 0x20
+LiquidCrystal_I2C lcd(0x27, 16, 2); //  เป็นการตั้งค่า ของจอ Lcd (0*27 คือขนาดของจอ,16 ตัวอักษร ,2 บรรทัด)
+
 char keymap[19] = "123A456B789C*0#DNF"; //  เป็นคำสั่งใช้ตัวแปร char โดยชื่อ keymap เป็นตัวเก็บจำนวนไว้ที่ตัวแปร ของ array
-                                        // สร้างออบเจ็ค Keypad_I2C
-String inputTime = "";                  //  ตัวแปร  ค่าล่าสุด
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+int lcdRow = 0;
+String inputTime = ""; //  ตัวแปร  ค่าล่าสุด
 String latestValue = "";
 bool lockKeypad; //  ตัวแปร  ล็อคปุ่มกด
 int menu;
@@ -19,39 +22,8 @@ bool pass;
 bool flagMenuChange;
 bool flagCommit;
 bool flagSendCmd;
-const uint64_t pipe = 0xE8E8F0F0E1LL;
 
-void setup()
-{ // เริ่มต้นการทำงานของ I2C
-    // Wire.begin();
-    // Wire.setClock(400000); // เป็นคำสั่งตั้งค่าความเร็วในการสื่อสาร (400000 fast mode )
-    lcd.init();
-    lcd.backlight();
-
-    Wire.setWireTimeout(1000, false);
-
-    Serial.begin(115200);
-
-    if (!radio.begin())
-    {
-        Serial.println("radio hardware is not responding!!!");
-        while (1)
-            ;
-    }
-
-    radio.openWritingPipe(pipe);
-
-    if (!keypad.begin()) //  ถ้า (keypad.begin เป็นการตรวจสอบว่าสื่อสารกันได้) keypad เป็น เท็จ
-    {
-        lcd.println("keypadError");
-        while (1)
-            ; //  เป็นคำสั่งทำซํ้าตลอดไปไม่หยุด
-    }
-    keypad.loadKeyMap(keymap); //  เป็นการตั้งค่า layout ของ keypad เป็นการดึงค่าจาก keymap มา
-
-    flagMenuChange = true;
-}
-
+// Functions
 void selectMenu(char buttonValue)
 {
     if (buttonValue < 'A' || buttonValue > 'C')
@@ -80,6 +52,7 @@ void selectMenu(char buttonValue)
     {
         menu = 3;
         pass = !pass;
+        flagSendCmd = true;
         return;
     }
     case 'D':
@@ -114,33 +87,12 @@ void checkConfirm(char buttonValue)
     {
         flagMenuChange = true;
 
-        if (flagCommit && inputTime.length() >= 6)
+        if ((flagCommit && inputTime.length() >= 6) || menu == 4)
         {
+            flagSendCmd = true;
             flagCommit = false;
-
-            int stringLength = 9;
-            char byteToSend[stringLength];
-
-            String dataToSend = String(menu) + "#";
-            if (menu == 1 || menu == 2)
-            {
-                dataToSend += inputTime;
-            }
-            else if (menu == 3)
-            {
-                dataToSend = String(pass);
-            }
-            else if (menu == 4)
-            {
-                dataToSend = "000000";
-            }
-
-            dataToSend.toCharArray(byteToSend, stringLength);
-
-            radio.write(byteToSend, stringLength);
-            Serial.println("Send Radio: " + dataToSend);
             return;
-        }        
+        }
 
         if (inputTime.length() >= 6)
             flagCommit = true;
@@ -151,6 +103,73 @@ void checkConfirm(char buttonValue)
         inputTime = "";
         flagCommit = false;
     }
+}
+
+void showHomeMenu()
+{
+    lcd.print("  CLOCK REMOTE  ");
+    lcd.setCursor(0, 1);
+    lcd.print("PRESS MENU A - D");
+}
+
+void showTimerMenu()
+{
+    if (flagCommit)
+    {
+        lcd.print(" PRESS # TO RUN ");
+        lcd.setCursor(0, 1);
+        lcd.print("     " + inputTime + "      ");
+        latestValue = inputTime;
+        return;
+    }
+
+    lcd.print("TIMER    " + inputTime);
+    latestValue = inputTime;
+}
+
+void showClockMenu()
+{
+    if (flagCommit)
+    {
+        lcd.print(" PRESS # TO SET ");
+        lcd.setCursor(0, 1);
+        lcd.print("     " + inputTime + "      ");
+        Serial.println("LCD Display: " + inputTime);
+        latestValue = inputTime;
+        return;
+    }
+
+    lcd.print("CLOCK    " + inputTime);
+    Serial.println("Menu 2");
+    latestValue = inputTime;
+}
+
+void showPauseMenu()
+{
+    if (pass)
+    {
+        lcd.print("   STOP TIMER   ");
+        return;
+    }
+
+    lcd.print("   RUN  TIMER   ");
+}
+
+void showResetMenu()
+{
+    if (flagCommit)
+    {
+        lcd.print("   RESET DONE   ");
+        menu = 0;
+        delay(3000);
+        latestValue = inputTime;
+        return;
+    }
+
+    lcd.print("    PRESS  #    ");
+    lcd.setCursor(0, 1);
+    lcd.print(" TO RESET TIMER ");
+    latestValue = inputTime;
 }
 
 void showMenu()
@@ -167,81 +186,119 @@ void showMenu()
     switch (menu)
     {
     case 0:
-        lcd.print("  CLOCK REMOTE  ");
-        lcd.setCursor(0, 1);
-        lcd.print("PRESS MENU A - D");
+        showHomeMenu();
         return;
     case 1:
-        if (flagCommit)
-        {
-            lcd.print(" PRESS # TO RUN ");
-            lcd.setCursor(0, 1);
-            lcd.print("     " + inputTime + "      ");
-            Serial.println("LCD Display: " + inputTime);
-            latestValue = inputTime;
-            return;
-        }
-
-        lcd.print("TIMER    " + inputTime);
-        Serial.println("Menu 1");
-        latestValue = inputTime;
+        showTimerMenu();
         return;
     case 2:
-        if (flagCommit)
-        {
-            lcd.print(" PRESS # TO SET ");
-            lcd.setCursor(0, 1);
-            lcd.print("     " + inputTime + "      ");
-            Serial.println("LCD Display: " + inputTime);
-            latestValue = inputTime;
-            return;
-        }
-
-        lcd.print("CLOCK    " + inputTime);
-        Serial.println("Menu 2");
-        latestValue = inputTime;
+        showClockMenu();
         return;
     case 3:
-        if (pass)
-        {
-            lcd.print("   STOP TIMER   ");
-            return;
-        }
-
-        lcd.print("   RUN  TIMER   ");
+        showPauseMenu();
+        return;
+    case 4:
+        showResetMenu();
         return;
     default:
         break;
     }
 }
 
+void readKeypad()
+{
+    bool pressed = keypad.isPressed();
+
+    if (pressed && !lockKeypad)
+    {
+        char keypadValue = keypad.getChar();
+
+        lockKeypad = true;
+        Serial.println("Keypad Lock: " + String(lockKeypad));
+
+        checkNumberValue(keypadValue);
+        checkConfirm(keypadValue);
+        selectMenu(keypadValue);
+    }
+
+    if (!pressed && lockKeypad)
+    {
+        lockKeypad = false;
+        Serial.println("Keypad Lock: " + String(lockKeypad));
+    }
+}
+
+void sendRadio()
+{
+    if (!flagSendCmd)
+        return;
+
+    if (menu > 4)
+    {
+        Serial.println("Send Radio: Error");
+        Serial.println("Invalid Menu Type: " + String(menu));
+        flagSendCmd = false;
+        return;
+    }
+
+    int stringLength = 9;
+
+    String dataToSend = String(menu) + "#";
+    if (menu == 1 || menu == 2)
+        dataToSend += inputTime;
+    else if (menu == 3)
+        dataToSend = "00000" + String(pass);
+    else
+        dataToSend = "000000";
+
+    char byteToSend[stringLength];
+    dataToSend.toCharArray(byteToSend, stringLength);
+
+    radio.write(byteToSend, stringLength);
+
+    Serial.println("Send Radio: " + dataToSend);
+
+    flagSendCmd = false;
+}
+
+// Run
+void setup()
+{
+    lcd.init();
+    lcd.backlight();
+
+    Wire.setWireTimeout(1000, false);
+
+    Serial.begin(115200);
+
+    if (!radio.begin())
+    {
+        Serial.println("radio hardware is not responding!!!");
+        while (1)
+            ;
+    }
+
+    radio.openWritingPipe(pipe);
+
+    if (!keypad.begin()) //  ถ้า (keypad.begin เป็นการตรวจสอบว่าสื่อสารกันได้) keypad เป็น เท็จ
+    {
+        lcd.println("keypadError");
+        while (1)
+            ; //  เป็นคำสั่งทำซํ้าตลอดไปไม่หยุด
+    }
+    keypad.loadKeyMap(keymap); //  เป็นการตั้งค่า layout ของ keypad เป็นการดึงค่าจาก keymap มา
+
+    flagMenuChange = true;
+}
+
 void loop()
 {
     if (millis() % 200 == 0)
-    {
-        bool pressed = keypad.isPressed();
+        readKeypad();
 
-        if (pressed && !lockKeypad)
-        {
-            char keypadValue = keypad.getChar();
-
-            lockKeypad = true;
-            Serial.println("Keypad Lock: " + String(lockKeypad));
-
-            checkNumberValue(keypadValue);
-            checkConfirm(keypadValue);
-            selectMenu(keypadValue);
-        }
-
-        if (!pressed && lockKeypad)
-        {
-            lockKeypad = false;
-            Serial.println("Keypad Lock: " + String(lockKeypad));
-        }
-    }
+    if (millis() % 100 == 0)
+        sendRadio();
 
     if (millis() % 350 == 0)
-    {
         showMenu();
-    }
 }
