3554aa6cd0694c90c7346daca6fd59c9f8982b4d
ungoodman <suchanej.19@gmail.com>
2024-06-14 22:20:09 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -366,48 +366,60 @@
 // }
 
 #include <Arduino.h>
-#pragma GCC optimize ("O3")   //code optimisation controls - "O2" & "O3" code performance, "Os" code size
+#pragma GCC optimize("O3") // code optimisation controls - "O2" & "O3" code performance, "Os" code size
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <I2CKeyPad.h>
+// #include <Keypad.h>
+// #include <SPI.h>
+// #include <RF24.h>
 
-#define COLUMS           20   //LCD columns
-#define ROWS             4    //LCD rows
-#define LCD_SPACE_SYMBOL 0x20 //space symbol from LCD ROM, see p.9 of GDM2004D datasheet
+#define COLUMS 20             // LCD columns
+#define ROWS 4                // LCD rows
+#define LCD_SPACE_SYMBOL 0x20 // space symbol from LCD ROM, see p.9 of GDM2004D datasheet
 
+I2CKeyPad keyPad(0x20);
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 void setup()
 {
-  Serial.begin(115200);
+    Wire.begin();
+    Serial.begin(115200);
 
-  while (lcd.begin(COLUMS, ROWS, LCD_5x8DOTS) != 1) //colums, rows, characters size
-  {
-    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal."));
-    delay(5000);   
-  }
+    Wire.setClock(400000);
+    if (keyPad.begin() == false)
+    {
+        Serial.println("\nERROR: cannot communicate to keypad.\nPlease reboot.\n");
+        while (1);
+    }
 
-  lcd.print(F("PCF8574 is OK..."));    //(F()) saves string to flash & keeps dynamic memory free
-  delay(2000);
+    while (lcd.begin(COLUMS, ROWS, LCD_5x8DOTS) != 1) // colums, rows, characters size
+    {
+        Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal."));
+        delay(5000);
+    }
 
-  lcd.clear();
+    lcd.print(F("PCF8574 is OK...")); //(F()) saves string to flash & keeps dynamic memory free
+    delay(2000);
 
-  /* prints static text */
-  lcd.setCursor(0, 1);                 //set 1-st colum & 2-nd row
-  lcd.print(F("Hello world!"));
+    lcd.clear();
 
-  lcd.setCursor(0, 2);
-  lcd.print(F("Random number:"));
+    /* prints static text */
+    lcd.setCursor(0, 1); // set 1-st colum & 2-nd row
+    lcd.print(F("Hello world!"));
+
+    lcd.setCursor(0, 2);
+    lcd.print(F("Random number:"));
 }
-
 
 void loop()
 {
-  /* print dynamic text */
-  lcd.setCursor(14, 2);        //set 15-th colum & 3-rd row
+    /* print dynamic text */
+    lcd.setCursor(14, 2); // set 15-th colum & 3-rd row
 
-  lcd.print(random(10, 1000));
-  lcd.write(LCD_SPACE_SYMBOL); //"write()" is faster than "lcd.print()"
+    lcd.print(random(10, 1000));
+    lcd.write(LCD_SPACE_SYMBOL); //"write()" is faster than "lcd.print()"
 
-  delay(1000);
+    delay(1000);
 }
