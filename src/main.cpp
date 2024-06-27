889f6f6967e63f2843d284e83cd8227f0ae01d9c
ungoodman <suchanej.19@gmail.com>
2024-06-28 00:03:55 +0700

    add radio

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <RF24.h>
 #pragma GCC optimize("O3") // code optimisation controls - "O2" & "O3" code performance, "Os" code size
+
+#define SERIAL_BAUD_RATE 115200
 
 #define LATCH_PIN 4
 #define DATA_PIN 5
 #define CLOCK_PIN 3
+
+#define PIPE_ADDRESS 0xE8E8F0F0E1LL
+#define CE_PIN 8
+#define CSN_PIN 7
 
 // digit 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 byte ledDigitBytes[] = {
@@ -18,20 +26,44 @@ byte ledDigitBytes[] = {
     B11111110,
     B11110110};
 
+byte error = B10011110;
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+uint32_t lastTime;
+uint32_t radioListenTime;
+int time[2] = {0, 0};
+bool flagCountDown = true;
+bool flagDisplayUpdate;
+
+void radioSetup() {
+    if (!radio.begin())
+    {
+        writeSegmentDigit(error);
+        writeSegmentDigit(ledDigitBytes[0]);
+        Serial.println(F("radio hardware is not responding!"));
+        while (true);
+    }
+
+    radio.openReadingPipe(1, PIPE_ADDRESS);
+    radio.startListening();
+    
+    Serial.println(F("radio setup: done"));
+}
+
 void setup()
 {
     pinMode(DATA_PIN, OUTPUT);
     pinMode(LATCH_PIN, OUTPUT);
     pinMode(CLOCK_PIN, OUTPUT);
 
-    Serial.begin(115200);
+    Serial.begin(SERIAL_BAUD_RATE);
 
-    Serial.println("Setup: done");
+    radioSetup();
 
-    Serial.println("Clock Start");
+    Serial.println("program setup: done");
+    Serial.println("program start");
 }
-
-uint32_t lastTime = 0;
 
 void writeSegmentDigit(byte value)
 {
@@ -39,9 +71,6 @@ void writeSegmentDigit(byte value)
     shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, value);
     digitalWrite(LATCH_PIN, HIGH);
 }
-
-int time[2] = {2, 0};
-bool flagCountDown = true;
 
 void timeTask() {
     if (!flagCountDown)
@@ -59,10 +88,12 @@ void timeTask() {
     {
         time[0] = 0;
     }
+
+    flagDisplayUpdate = true;
 }
 
 void showTime() {
-    if (!flagCountDown)
+    if (!flagDisplayUpdate)
         return;
 
     int toShow[4] = {time[0] / 10, time[0] % 10, time[1] / 10, time[1] % 10};
@@ -73,12 +104,45 @@ void showTime() {
         writeSegmentDigit(ledDigitBytes[index]);
     }
 
+    flagDisplayUpdate = false;
+    
     if (time[0] == 0 && time[1] == 0 && flagCountDown)
         flagCountDown = false;    
 }
 
+void listenRadio() {
+    if (!radio.available())
+        return;
+    
+    char message[10];
+    while (radio.available())
+        radio.read(&message, 10);
+
+    String messageStr = String(message);
+    int menu = messageStr.substring(0, 1).toInt();
+
+    if (menu == 1)
+    {
+        time[0] = messageStr.substring(3, 5).toInt();
+        time[1] = messageStr.substring(5, 7).toInt();
+
+        flagDisplayUpdate = true;
+    } else if (menu == 3)
+    {
+        flagCountDown = messageStr.substring(6, 7).toInt();
+    }
+}
+
 void loop()
 {
+    if (millis() - radioListenTime >= 500)
+    {
+        listenRadio();
+
+        radioListenTime = millis();
+    }
+    
+
     if (millis() - lastTime >= 1000)
     {
         showTime();
