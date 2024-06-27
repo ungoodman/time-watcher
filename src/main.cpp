3d28753f58232dfe51c9ffef0a347f83c05b8881
ungoodman <suchanej.19@gmail.com>
2024-06-27 22:43:31 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #pragma GCC optimize("O3") // code optimisation controls - "O2" & "O3" code performance, "Os" code size
 
-#define DATA_A_PIN 4
-#define DATA_B_PIN 5
+#define LATCH_PIN 4
+#define DATA_PIN 5
 #define CLOCK_PIN 3
 
 // digit 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
@@ -20,8 +20,8 @@ byte ledDigitBytes[] = {
 
 void setup()
 {
-    pinMode(DATA_A_PIN, OUTPUT);
-    pinMode(DATA_B_PIN, OUTPUT);
+    pinMode(DATA_PIN, OUTPUT);
+    pinMode(LATCH_PIN, OUTPUT);
     pinMode(CLOCK_PIN, OUTPUT);
 
     Serial.begin(115200);
@@ -33,17 +33,31 @@ void setup()
 
 uint32_t lastTime = 0;
 
-int i;
+void writeSegmentDigit(byte value)
+{
+    digitalWrite(LATCH_PIN, LOW);
+    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, value);
+    digitalWrite(LATCH_PIN, HIGH);
+}
+
+int x;
+
 void loop()
 {
-    if (millis() - lastTime >= 200)
+    if (millis() - lastTime >= 1000)
     {
-        shiftOut(DATA_A_PIN, CLOCK_PIN, LSBFIRST, B01100000);
-        shiftOut(DATA_A_PIN, CLOCK_PIN, LSBFIRST, B01100000);
+        for (int i = 0; i < 5; i++)
+        {
+            writeSegmentDigit(ledDigitBytes[x]);
+        }
+        
+        x++;
 
-        shiftOut(DATA_B_PIN, CLOCK_PIN, LSBFIRST, B11011010);
-        shiftOut(DATA_B_PIN, CLOCK_PIN, LSBFIRST, B11011010);
-
+        if (x > 9)
+        {
+            x = 0;
+        }
+        
         lastTime = millis();
     }
 }
